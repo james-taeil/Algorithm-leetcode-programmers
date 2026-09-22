b8cbbd1df5a1a36c4f567bb8b2e80c32815575fe
Tae-il Kim <roseswallow13@gmail.com>
2026-09-22 09:25:05 +0900

    [level 2] Title: 올바른 괄호, Time: 11.71 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        char[] ch = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : ch) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (stack.peek() == '(') {
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
